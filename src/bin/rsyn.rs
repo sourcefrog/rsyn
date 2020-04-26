908a0d79d5f18263220390c36bb2b725180ad90f
Martin Pool <mbp@sourcefrog.net>
2020-04-26 13:57:34 -0700

    Move default logging setup to the library

--- src/bin/rsyn.rs
+++ src/bin/rsyn.rs
@@ -5,25 +5,8 @@ use log::{debug, error, info, trace, warn};
 
 use rsyn::Connection;
 
-fn setup_logger() {
-    fern::Dispatch::new()
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "[{}][{}] {}",
-                record.target(),
-                record.level(),
-                message
-            ))
-        })
-        .level(log::LevelFilter::Debug)
-        .chain(std::io::stdout())
-        .chain(fern::log_file("rsyn.log").expect("failed to open log file"))
-        .apply()
-        .expect("failed to configure logger")
-}
-
 fn main() -> io::Result<()> {
-    setup_logger();
+    rsyn::default_logging();
     let mut conn = Connection::local_subprocess("/etc")?;
     conn.list_files()?;
     debug!("that's all folks");
--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,20 @@ mod varint;
 mod statistics;
 
 pub use connection::Connection;
+
+pub fn default_logging() {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "[{}][{}] {}",
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("rsyn.log").expect("failed to open log file"))
+        .apply()
+        .expect("failed to configure logger")
+}

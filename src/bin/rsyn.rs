2365bec7577263fbd02fb8f57e937a66a3c3e882
Martin Pool <mbp@sourcefrog.net>
2020-04-26 21:21:40 -0700

    List files in approximately ls -l format
    For now only decode mode; timestamp is still printed as a raw integer.

--- src/bin/rsyn.rs
+++ src/bin/rsyn.rs
@@ -9,7 +9,7 @@ fn main() -> io::Result<()> {
     rsyn::logging::default_logging();
     let file_list = Connection::local_subprocess("/etc")?.list_files()?;
     for entry in file_list {
-        println!("{}", String::from_utf8_lossy(&entry.name));
+        println!("{}", &entry)
     }
     debug!("that's all folks");
     Ok(())

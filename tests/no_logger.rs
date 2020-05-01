f1cc084e990077f6551178cdf6a3757743074ed7
Martin Pool <mbp@google.com>
2020-05-01 07:25:15 -0700

    Comment

--- tests/no_logger.rs
+++ tests/no_logger.rs
@@ -1,6 +1,10 @@
 use rsyn::Connection;
 
 /// Check that we're not counting on the side effects of any logging.
+///
+/// This must be in a different target from other interop tests, so that it
+/// runs in a different process, and doesn't accidentally inherit a global
+/// logger.
 #[test]
 fn list_files_with_no_logger() {
     // TODO: Assertions about the contents.
